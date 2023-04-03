aef5937aade5c49c1c9a7fec7b4f5a0d7f14ab24
Nastya Boykova <pacanovskayan@mail.ru>
2023-04-03 14:01:30 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// задача 1
+const janitorSalary = 40000;
+const janitorTeacher = 22000;
+const result = janitorSalary + janitorTeacher;
+console.log(`Результат 1 задачи: ${result}.`);
+
+// задача 2
+const word = " Владивосток ";
+const namber = 2000;
+const answer = word + namber;
+console.log(`Результат 2 задачи: ${answer}.`);
+
+// задача 3
+const messMinutes = 128;
+const likeMinutes = 254;
+const vkMinutes = 137;
+const youtubeMinutes = 201;
+const totalMinutes = messMinutes + likeMinutes + vkMinutes + youtubeMinutes;
+console.log(`Результат 3 задачи: ${totalMinutes} часов`);
+
+// задача 4
+console.log(2 ** 5);
+
+// задача 5
+console.log(' Нужно ' + ' бежать ' + ' со ' + ' всех ' + ' ног ' + ',' + ' чтобы ' + ' только ' + ' оставаться ' + ' на ' + ' месте ' + ',' + 'а' + ' чтобы ' + ' куда-то ' + ' попасть ' + ',' + ' надо ' + ' бежать ' + ' как ' + ' минимум ' + ' вдвое ' + ' быстрее ' + '!');
+
+// задача 6
+const greetings = "Привет";
+const name = " Кот ";
+console.log(`Результат 4 задачи: ${greetings + ',' + name}!`);
+
+// задача 7
+const time = 34;
+console.log(`Старт поездки. Осталось минут: ${time}.`);
+const stories = 15;
+console.log(`Немного сторис в соцсетях. Осталось минут: ${time - stories}.`);
+const news = 10;
+console.log(`Немного не новостей, но событий. Осталось минут: ${time - stories - news}.`);
+const timeEnd = 0;
+console.log(`Вы` + ` приехали` + `.` + ` Добро` + ` пожаловать` + ` в` + ` Москву` + `!`)
+
+// задача 8
+const fahrenheit = 451;
+const celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту — это ${celsius} градуса по Цельсию.`)
